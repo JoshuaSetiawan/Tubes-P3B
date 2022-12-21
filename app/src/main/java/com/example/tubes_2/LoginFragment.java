77043de052a9d1f4013a45158cb7e3ebc0781a4b
hizkia904 <115910774+hizkia904@users.noreply.github.com>
2022-12-21 17:12:57 +0700

    mencoba login dengan API

--- app/src/main/java/com/example/tubes_2/LoginFragment.java
+++ app/src/main/java/com/example/tubes_2/LoginFragment.java
@@ -1,18 +1,31 @@
 package com.example.tubes_2;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.tubes_2.databinding.LoginBinding;
+import com.google.gson.Gson;
 
 public class LoginFragment extends Fragment {
     LoginBinding loginBinding;
+    FragmentActivity fragmentActivity;
+    Gson gson;
     public LoginFragment(){
 
     }
@@ -22,6 +35,41 @@ public class LoginFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         loginBinding = LoginBinding.inflate(inflater,container,false);
         View view = loginBinding.getRoot();
+        fragmentActivity = getActivity();
+        gson = new Gson();
+        loginBinding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Base_URL = "https://ifportal.labftis.net/api/v1/authenticate";
+                LoginInput loginInput =new LoginInput(loginBinding.etEmail.getText().toString(),
+                        loginBinding.etPassword.getText().toString(),loginBinding.etRole.getText().toString());
+                String inputJson = gson.toJson(loginInput);
+                RequestQueue queue = Volley.newRequestQueue(fragmentActivity);
+                StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                        Base_URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        memprosesKeluaranBerhasil(response);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        memprosesKeluaranGagal(error);
+                    }
+                }){
+                    @Override
+                    public byte[] getBody() throws AuthFailureError {
+                        return inputJson.getBytes();
+                    }
+
+                    @Override
+                    public String getBodyContentType() {
+                        return "application/json";
+                    }
+                };
+                queue.add(stringRequest);
+            }
+        });
         return view;
 
     }
@@ -29,4 +77,16 @@ public class LoginFragment extends Fragment {
         LoginFragment fragment = new LoginFragment();
         return fragment;
     }
+    public void memprosesKeluaranBerhasil(String response){
+        LoginOutput loginOutput = gson.fromJson(response,LoginOutput.class);
+        String hasil = "BERHASIL LOGIN\nTOKEN:";
+        hasil+=loginOutput.token;
+        Toast.makeText(fragmentActivity,hasil,Toast.LENGTH_LONG).show();
+    }
+    public void memprosesKeluaranGagal(VolleyError error){
+        Toast.makeText(fragmentActivity,"GAGAL:"+new String(error.networkResponse.data),Toast.LENGTH_LONG).show();
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/tubes_2/LoginOutput.java
@@ -0,0 +1,8 @@
+package com.example.tubes_2;
+
+public class LoginOutput {
+    String token;
+    public LoginOutput(String token){
+        this.token = token;
+    }
+}
